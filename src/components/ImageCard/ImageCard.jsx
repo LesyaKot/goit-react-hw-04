b50c681ca997fb802b617006e997f0201f6a3816
LesyaKot <lesyakot83@gmail.com>
2024-04-18 11:24:58 +0500

    fix ImageCard

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -1,9 +1,12 @@
-import styles from './ImageCard.module.css'
+import styles from "./ImageCard.module.css";
 
 export default function ImageCard({ image, onClick }) {
   return (
-    <div className={styles.wrap} onClick={() => onClick(image.urls.small)}> 
-      <img className={styles.img} src={image.urls.small} alt={image.alt_description} />
-    </div>
+    <img
+      onClick={() => onClick(image.urls.small)}
+      className={styles.img}
+      src={image.urls.small}
+      alt={image.alt_description}
+    />
   );
 }
